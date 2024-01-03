e3141de7a52c643f67beb2e7790ec0c4bf08de6d
DV <divyanshsingh372@gmail.com>
2024-01-04 00:22:24 +0530

    Frontend completed

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,16 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
+
+const ChatsPage = (props) => {
+  return (
+    <div style = {{height: '100vh'}}>
+    <PrettyChatWindow
+      projectId="6a827937-58ca-468d-afee-61f4f010ade0"
+      username={props.user.username}
+      secret={props.user.secret}
+      style={{ height: '100vh' }}
+    />
+    </div>
+  )
 }
 
 export default ChatsPage
