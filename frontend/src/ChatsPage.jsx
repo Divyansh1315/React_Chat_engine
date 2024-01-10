1eef6b3af636ab8bd64f9af142e29e7ba3d06e84
DV <divyanshsingh372@gmail.com>
2024-01-10 20:53:17 +0530

    Chat page updated
    Few changes in code

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,16 +1,24 @@
-import { PrettyChatWindow } from 'react-chat-engine-pretty';
+import {
+    ChatEngine,
+    MultiChatSocket,
+    useMultiChatLogic,
+  } from 'react-chat-engine-advanced';
 
 const ChatsPage = (props) => {
-  return (
+    const chatProps = useMultiChatLogic(
+        '6a827937-58ca-468d-afee-61f4f010ade0',
+        props.user.username,
+        props.user.secret
+      );
+
+return(
     <div style = {{height: '100vh'}}>
-    <PrettyChatWindow
-      projectId="6a827937-58ca-468d-afee-61f4f010ade0"
-      username={props.user.username}
-      secret={props.user.secret}
-      style={{ height: '100vh' }}
-    />
+        <MultiChatWindow {...chatProps} />
+      <MultiChatSocket {...chatProps} style = {{height: '100%'}} />
+      
     </div>
-  )
+)
+  
 }
 
 export default ChatsPage
